5adcc6f6d5ea577ccc7f9ef3be3bfa5e70c06cf4
Olivier Lamy <olamy@apache.org>
2011-02-08 21:58:02 +0100

    add same it but when using forking

---
+++ src/it/full-test-fork/src/test/java/org/jenkinsci/AppTest.java
@@ -0,0 +1,35 @@
+package org.jenkinsci;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+    
+    public void testMine()
+    {
+        assertTrue( true );
+    }    
+}
---
+++ src/it/regextest-fork/src/test/java/org/jenkinsci/AppTest.java
@@ -0,0 +1,44 @@
+package org.jenkinsci;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+    
+    /**
+     * Rigourous Test :-)
+     */
+    public void testAppOneFoo()
+    {
+        assertTrue( true );
+    }
+    
+    public void testAppTwoFoo()
+    {
+        assertTrue( true );
+    }    
+}
